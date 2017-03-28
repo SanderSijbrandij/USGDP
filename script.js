259aab544de7b304d8a19bf74051c555e0d3be17
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-28 15:26:46 +0200

    Visualize the data

--- script.js
+++ script.js
@@ -3,22 +3,41 @@ document.addEventListener('DOMContentLoaded', () => {
   getData(dataFile)
     .catch((err) => { console.error('something went wrong') })
     .then((res) => {
-      console.log(res)
+      // necessary data
+      const datapoints = res.data.length
+      const minGDPValue = d3.min(res.data, d => d[1])
+      const maxGDPValue = d3.max(res.data, d => d[1])
+      const minDateValue = d3.min(res.data, d => d[0])
+      const maxDateValue = d3.max(res.data, d => d[0])
+      const minDateYear = minDateValue.split('').slice(0,4).join('')
+      const maxDateYear = maxDateValue.split('').slice(0,4).join('')
 
+      console.log('Lowest: ', minDateYear, minGDPValue)
+      console.log('Highest: ', maxDateYear, maxGDPValue)
       // set sizes
-      const w = 800, h = 400, p = 2
+      const p = 40
+      const h = 400
+      const w = 2*datapoints + 2*p
+      const sizes = { w, h, p }
 
       // Create the Chart
       createChart(w, h)
 
-      // get min values
-      // get max values
-      // set scale & domain
-      // Add x Axis
+      // set scale & domain - Y axis
+      const scaleY = d3.scaleLinear()
+      scaleY.domain([minGDPValue, maxGDPValue])
+      scaleY.range([h-p, 0])
+
+      const scaleX = d3.scaleLinear()
+      scaleX.domain([minDateYear, maxDateYear])
+      scaleX.range([0, w-2*p])
+
       // add y Axis
+      createYAxis(scaleY, sizes)
+      createXAxis(scaleX, sizes)
 
       // Add bars
-      createBars(res.data, w, h, p)
+      createBars(res.data, scaleY, sizes)
 
     })
 })
@@ -48,15 +67,36 @@ const createChart = (w, h) => {
 
 // Add the bars
 // TODO: Actually use the data
-const createBars = (data, width, height, padding) => {
+const createBars = (data, scaleY, sizes) => {
+  const { h, p } = sizes
   d3.select('#chart')
     .selectAll('rect')
       .data(data)
       .enter()
       .append('rect')
       .attr('class', 'bar')
-      .attr('width', 15)
-      .attr('height', 300)
-      .attr('x', (d, i) => padding + (i * (15 + padding)))
-      .attr('y', height-300)
+      .attr('width', 2)
+      .attr('height', (d) => h - p - scaleY(d[1]))
+      .attr('x', (d, i) => p + (i * 2))
+      .attr('y', (d) => scaleY(d[1]) + (p/2))
+}
+
+const createYAxis = (scaleY, sizes) => {
+  const { w, h, p } = sizes
+  const yAxis = d3.axisLeft(scaleY)
+
+  d3.select('#chart')
+    .append('g')
+    .attr('transform', `translate(${p}, ${p/2})`)
+    .call(yAxis)
+}
+
+const createXAxis = (scaleX, sizes) => {
+  const { w, h, p } = sizes
+  const xAxis = d3.axisBottom(scaleX)
+
+  d3.select('#chart')
+    .append('g')
+    .attr('transform', `translate(${p}, ${h - (p/2)})`)
+    .call(xAxis)
 }
