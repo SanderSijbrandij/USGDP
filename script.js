abf6d61a8c733501d158a3feeeac80bfcdc8329c
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-28 12:29:32 +0200

    get the data from the API

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//  data: https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json
+document.addEventListener('DOMContentLoaded', () => {
+  const dataFile = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+  getData(dataFile)
+    .catch((err) => { console.error('something went wrong') })
+    .then((res) => {
+      // call anything that requires the data here.
+      // This is the point where it's fetched.
+      console.log(res)
+    })
+})
+
+// Create a Promise so we can have a bit of error handling
+// this fetches the data from the url passed to it.
+const getData = (url) => {
+  return new Promise((resolve, reject) => {
+    var req = new XMLHttpRequest()
+    req.open("GET", url, true)
+    req.send()
+    req.onload = () => {
+      if (req.status == 200) { resolve(JSON.parse(req.responseText)) }
+      else { reject(req.statusText) }
+    }
+  })
+}
