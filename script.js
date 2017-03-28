b125cfd8144955e6421fab9826271a81419116fc
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-28 13:35:24 +0200

    add bar for each data point

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
   getData(dataFile)
     .catch((err) => { console.error('something went wrong') })
     .then((res) => {
-      // call anything that requires the data here.
-      // This is the point where it's fetched.
       console.log(res)
-      createChart();
+
+      // set sizes
+      const w = 800, h = 400, p = 2
+
+      // Create the Chart
+      createChart(w, h)
+
+      // get min values
+      // get max values
+      // set scale & domain
+      // Add x Axis
+      // add y Axis
+
+      // Add bars
+      createBars(res.data, w, h, p)
+
     })
 })
 
@@ -25,14 +38,25 @@ const getData = (url) => {
 }
 
 // create the SVG
-const createChart = () => {
-  const padding = 100,
-        w = 800,
-        h = 400
-
+const createChart = (w, h) => {
   d3.select('body')
     .append('svg')
     .attr('id', 'chart')
     .attr('width', w)
     .attr('height', h)
+}
+
+// Add the bars
+// TODO: Actually use the data
+const createBars = (data, width, height, padding) => {
+  d3.select('#chart')
+    .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('width', 15)
+      .attr('height', 300)
+      .attr('x', (d, i) => padding + (i * (15 + padding)))
+      .attr('y', height-300)
 }
